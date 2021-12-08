b7eae5ef13091a0561b4980a22902352c3c9df46
Guilherme <guijpjs@gmail.com>
2021-12-07 23:48:11 -0300

    chore ✨ : Add uuid infilm

---
+++ src/routes/films.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { v4 as uuidV4 } from 'uuid';
+
+const filmsRoutes = Router();
+
+const films = [];
+
+filmsRoutes.post('/create', (request, response) => {
+  const {
+    name, director, cast, year, gender,
+  } = request.body;
+
+  const film = {
+    id: uuidV4(),
+    name,
+    director,
+    cast,
+    year,
+    gender,
+  };
+
+  films.push(film);
+
+  return response.status(201).send();
+});
+
+export { filmsRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import express from 'express';
 
+import { filmsRoutes } from './routes/films.routes';
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => response.json({ message: 'Olá Mu2ndo' }));
-
-app.post('/film', (request, response) => {
-  console.log(request.body);
-  const { name, email } = request.body;
-  return response.json({ name });
-});
+app.use('/films', filmsRoutes);
 
 app.listen(3333, () => console.log('Server started on port 3333'));
