9de96c07bfc01ec81e6536b30ff6f8e6b7ebdf92
Guilherme <guijpjs@gmail.com>
2021-12-07 23:32:41 -0300

    base 🔌  : Add  debugmode

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => response.json({ message: 'Olá Mu2ndo' }));
+
+app.post('/film', (request, response) => {
+  console.log(request.body);
+  const { name, email } = request.body;
+  return response.json({ name });
+});
 
 app.listen(3333, () => console.log('Server started on port 3333'));
