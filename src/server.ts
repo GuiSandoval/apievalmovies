35964ab58e4cd983a0fde405a1f395f58f432bd7
Guilherme <guijpjs@gmail.com>
2021-12-08 23:08:16 -0300

    chore ✨ : Add Cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import './database';
 
 import { filmsRoutes } from './routes/films.routes';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/films', filmsRoutes);
